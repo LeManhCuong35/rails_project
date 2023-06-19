f20cecea5156de62bc41123ea3d87f933baa014e
LeManhCuong35 <le.manh.cuong-c@sun-asterisk.com>
2023-06-19 10:56:39 +0700

    chapter 9

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "static_pages/home"
   get "static_pages/about"
   scope "(:locale)", locale: /en|vi/ do
-    resources :users, only: %i(new create show)
+    resources :users
     resources :articles
     get "/", to: "welcome#index"
 
